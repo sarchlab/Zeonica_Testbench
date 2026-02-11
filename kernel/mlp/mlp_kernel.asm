c3ab3709d7ed5ff9e1a67c6fc8a23b1e9894af5d
YIJIA SHI <37942719+n0thingNoob@users.noreply.github.com>
2026-02-11 23:54:02 +0000

    update mlp

---
+++ kernel/mlp/mlp_kernel.asm
@@ -0,0 +1,513 @@
+# Compiled II: 2
+
+PE(0,0):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(1,0):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(2,0):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(3,0):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(4,0):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(5,0):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(6,0):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(7,0):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(0,1):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(1,1):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(2,1):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(3,1):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(4,1):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(5,1):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(6,1):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(7,1):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(0,2):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(1,2):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(2,2):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(3,2):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(4,2):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(5,2):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(6,2):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(7,2):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(0,3):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(1,3):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(2,3):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(3,3):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(4,3):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(5,3):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(6,3):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(7,3):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(0,4):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(1,4):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(2,4):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(3,4):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(4,4):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(5,4):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(6,4):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(7,4):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(0,5):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(1,5):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(2,5):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(3,5):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(4,5):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(5,5):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(6,5):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(7,5):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(0,6):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(1,6):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(2,6):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(3,6):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(4,6):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(5,6):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(6,6):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(7,6):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(0,7):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(1,7):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(2,7):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(3,7):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(4,7):
+{
+  ICMP_SGT, [WEST, RED], [#0] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  SEL, [$0], [WEST, RED], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(5,7):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(6,7):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
+
+PE(7,7):
+{
+  MUL, [WEST, RED], [NORTH, RED] -> [$0] (t=0, inv_iters=0)
+} (idx_per_ii=0)
+{
+  ADD, [$0], [#0] -> [EAST, RED] (t=1, inv_iters=0)
+} (idx_per_ii=1)
