a8bcada8aaf264e81ea4da23019206aa92bcc3e2
n0thingNoob <yshi16@wm.edu>
2026-03-22 14:43:10 +0000

    update conv

--- kernel/conv/tmp-generated-instructions.asm
+++ kernel/conv/tmp-generated-instructions.asm
@@ -2,6 +2,13 @@
 
 PE(2,0):
 {
+  GRANT_PREDICATE, [NORTH, RED], [$0] -> [$0] (t=11, inv_iters=2)
+} (idx_per_ii=1)
+{
+  DATA_MOV, [NORTH, RED] -> [$0] (t=7, inv_iters=1)
+  RETURN_VALUE, [$0] (t=12, inv_iters=2)
+} (idx_per_ii=2)
+{
   DATA_MOV, [EAST, RED] -> [NORTH, RED] (t=9, inv_iters=1)
 } (idx_per_ii=4)
 
@@ -24,15 +31,11 @@ PE(1,1):
 
 PE(2,1):
 {
-  DATA_MOV, [NORTH, RED] -> [$1] (t=5, inv_iters=1)
-  ADD, [SOUTH, RED], [$0] -> [WEST, RED], [$0] (t=10, inv_iters=2)
+  ADD, [SOUTH, RED], [$0] -> [WEST, RED], [SOUTH, RED] (t=10, inv_iters=2)
 } (idx_per_ii=0)
 {
-  GRANT_PREDICATE, [$0], [$1] -> [$0] (t=11, inv_iters=2)
+  DATA_MOV, [NORTH, RED] -> [SOUTH, RED] (t=6, inv_iters=1)
 } (idx_per_ii=1)
-{
-  RETURN_VALUE, [$0] (t=12, inv_iters=2)
-} (idx_per_ii=2)
 {
   DATA_MOV, [NORTH, RED] -> [$0] (t=9, inv_iters=1)
 } (idx_per_ii=4)
@@ -61,10 +64,14 @@ PE(1,2):
 {
   DATA_MOV, [EAST, RED] -> [$0] (t=3, inv_iters=0)
 } (idx_per_ii=3)
+{
+  DATA_MOV, [EAST, RED] -> [EAST, RED] (t=4, inv_iters=0)
+} (idx_per_ii=4)
 
 PE(2,2):
 {
   GRANT_PREDICATE, [$1], [$0] -> [$0] (t=5, inv_iters=1)
+  DATA_MOV, [WEST, RED] -> [SOUTH, RED] (t=5, inv_iters=1)
 } (idx_per_ii=0)
 {
   PHI_START, [EAST, RED], [$0] -> [EAST, RED], [$0] (t=1, inv_iters=0)
@@ -74,12 +81,11 @@ PE(2,2):
   DATA_MOV, [EAST, RED] -> [WEST, RED] (t=2, inv_iters=0)
 } (idx_per_ii=2)
 {
-  ICMP_EQ, [$0], [#4200] -> [$0], [$2] (t=3, inv_iters=0)
+  ICMP_EQ, [$0], [#4200] -> [$0], [WEST, RED] (t=3, inv_iters=0)
   DATA_MOV, [WEST, RED] -> [SOUTH, RED] (t=8, inv_iters=1)
 } (idx_per_ii=3)
 {
   NOT, [$0] -> [WEST, RED], [$0] (t=4, inv_iters=0)
-  DATA_MOV, [$2] -> [SOUTH, RED] (t=4, inv_iters=0)
 } (idx_per_ii=4)
 
 PE(3,2):
